a74709c2063f63d7e67d104c6519e1747f840e36
Vedant Rao <vedantatwork21@gmail.com>
2024-01-05 18:44:14 +0530

    objects

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Jjhbcxz');
+//objects
+
+let name = "vedant"
+let age = 70;
+let hobby = 'reading';
+
+let user = {
+name : 'Vedant',
+age : 70,
+hobby :'reading',
+
+}
+console.log (user);
+console.log(user,name)
+console.log(user,hobby)
+
+console.log(user['name']);
+console.log(user['age']);
